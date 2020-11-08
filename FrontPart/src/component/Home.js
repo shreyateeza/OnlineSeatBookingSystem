48a0e0fb919478c5caeb447231fea35f54b11720
Nikhil Betke <NikhilPrabhakar.Betke@team.telstra.com>
2020-11-08 14:54:07 +0530

    updated homepage

--- FrontPart/src/component/Home.js
+++ FrontPart/src/component/Home.js
@@ -1,29 +1,38 @@
 import React from "react";
+import bg1 from "../images/bg5.jpg"
 
 function Home() {
     return (
 
         <>
-        <div style={{margin:'auto'}}><br/><br/><br/><br/>
-             <h1 class="display-5"><b><big>SEAT BOOKING APP </big></b></h1> <br/><br/><br/> 
+        <div style={{margin:'auto', backgroundImage:"url(" + bg1 +")", backgroundRepeat:'no-repeat', backgroundSize:'100%' ,  height:'100%', width:'100%', position:'absolute'}}>
+            <br/>
+            <header>
+             <h1 class="display-5"><b><big>
+                 {/* <i id='lg' class='fas fa-cannabis'/> */}
+                 <i id='lg' class='fab fa-centercode'/>
+                 {/* <i id='lg' class='fas fa-angle-double-right'/> */}
+                 Seat Booking App </big></b></h1>
+            </header>
+             <br/><br/><br/><br/><br/><br/> 
                 <div class="d-flex justify-content-center mt-2">
                  
-                    <a href="/login">
-                    <div class="d-inline-flex m-2 w-15">
+                    <a id='a' href="/login">
+                    <div id = 'tile' class="d-inline-flex m-2 w-15">
                     <h5>
                     <i class="fas fa-sign-in-alt m-2 fa-2x" />
                     <br/>
-                            Login 
+                    &nbsp;&nbsp;&nbsp; Login &nbsp;&nbsp;&nbsp; 
                     </h5>
                     </div>
                     </a>
 
-                    <a href="/signup">
-                    <div class="d-inline-flex m-2 w-15">
+                    <a id='a' href="/signup">
+                    <div id = 'tile' class="d-inline-flex m-2 w-15">
                     <h5>
                     <i class="fas fa-user-plus m-2 fa-2x " />
                     <br/>
-                            Sign Up
+                    &nbsp;  Sign Up &nbsp;
                     </h5>
                     </div>
                     </a>
